691f6e2519abd0ea2428d3d5f21ae419ee4eff04
Luke Hindman <lukehindman@boisestate.edu>
2020-10-29 15:35:04 -0700

    Intermediate commit...

--- File Basics/main.c
+++ File Basics/main.c
@@ -2,7 +2,21 @@
 
 int main(void) {
 
-   /* Your code here */
+   /* Similar to how we can use the stdin, stdout and stderr FILE streams
+    *   to read from and write to the console, we can also create our
+    *   own FILE streams to fread from and write to files.
+    */
+
+   FILE * mydoc = fopen("sample_data.txt", "r");
+
+   char buffer[80];
+   if (fscanf(mydoc,"%79[^\n]",buffer) > 0) {
+      fprintf(stdout,"I successfully read the following from the data file:\n%s\n", buffer);
+   } else {
+      fprintf(stderr,"ERROR: Unable to content from file");
+   }
+
+   
 
    return 0;
 }
